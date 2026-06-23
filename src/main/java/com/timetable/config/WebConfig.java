1575d1b5f339b4f8a2516ac369562120f7a17804
Annu Theresa <annutheres@gmail.com>
2026-06-23 10:12:10 +0530

    allow vercel access

--- src/main/java/com/timetable/config/WebConfig.java
+++ src/main/java/com/timetable/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000")
+                        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000","https://timetable-backend-pqq5.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
