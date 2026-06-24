801b60409dc2de2c4ee7f24724b0439ce06afb3f
Annu Theresa <annutheres@gmail.com>
2026-06-24 13:47:28 +0530

    changed vercel url

--- src/main/java/com/timetable/config/WebConfig.java
+++ src/main/java/com/timetable/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000","https://*.vercel.app")
+                        .allowedOriginPatterns("http://localhost:3000", "http://127.0.0.1:3000","https://*.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
